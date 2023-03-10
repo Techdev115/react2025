0bf74e99ddd91c6381893edae2da0fe3e722db1d
TechDev <happysuperstar601@gmail.com>
2023-03-10 09:51:23 -0500

    28

---
+++ docs/next.config.js
@@ -0,0 +1,13 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  unstable_stork: true,
+  unstable_staticImage: true
+});
+
+module.exports = withNextra({
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en'
+  }
+});
